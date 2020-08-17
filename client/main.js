f175ae12d050f47664cd4b57bf5744d28595ab7e
elicolh <36312455+elicolh@users.noreply.github.com>
2020-08-17 23:13:18 +0200

    content

---
+++ client/main.js
@@ -0,0 +1,36 @@
+const request = require('request');
+const fs = require("fs")
+var args = process.argv.slice(2);
+
+var savePath = args[args.findIndex(e=>e=="--path")+1]
+if(!fs.existsSync(savePath))return
+
+var ordre = args[args.findIndex(e=>e=="--rs")+1]
+if( ordre == "recup")
+    recup()
+else if(ordre == "save")
+    save()
+
+
+function recup(){
+    var filePath = savePath + "\\save.zip"
+    request
+    .get('http://localhost:7412/dl')
+    .on('error', (err)=>console.error(err))
+    .pipe(fs.createWriteStream(filePath))
+    .on("finish",()=>{
+        console.log("dl finit")
+    })
+}
+
+function save(){
+    var req = request.post('http://localhost:7412/save', function (err, resp, body) {
+        if (err) {
+          console.log('Error!');
+        } else {
+          console.log('URL: ' + body);
+        }
+      });
+      var form = req.form();
+      form.append('file', fs.createReadStream(savePath + "\\save.zip"));
+}
---
+++ server/main.js
@@ -0,0 +1,47 @@
+const express = require("express")
+const bodyParser = require("body-parser")
+var busboy = require('connect-busboy'); //middleware for form/file upload
+var fs = require("fs")
+const app = express();
+app.use(busboy());
+app.use(express.static("public"));
+app.use(bodyParser.json()); // to support JSON-encoded bodies
+app.use(
+  bodyParser.urlencoded({
+    // to support URL-encoded bodies
+    extended: true
+  })
+)
+
+app.route('/save')
+    .post(function (req, res, next) {
+        var fstream;
+        req.pipe(req.busboy);
+        req.busboy.on('file', function (fieldname, file, filename) {
+            console.log("Uploading: " + filename);
+
+            //Path where image will be uploaded
+            var filePath = __dirname + "\\saves\\save" + Date().toLocaleString().replace(/ /gi, '_').replace(/:/gi, '.').replace(/\//gi,'-') + ".zip"
+            fstream = fs.createWriteStream(filePath);
+            file.pipe(fstream);
+            fstream.on('close', function () {    
+                console.log("Upload Finished of " + filename);              
+                res.redirect('back');           //where to go next
+                fs.copyFile(filePath, __dirname + "\\lastSave.zip",()=>console.log("done"))
+            });
+        });
+    })
+
+app.get("/dl",(req,res)=>{
+    console.log("req")
+    res.download(__dirname + "\\lastSave.zip")
+    // res.download(__dirname + "\\save.txt")
+})
+
+
+
+app.post("/gitChange",(req,res)=>{
+
+})
+
+app.listen("7412",()=>console.log("listening"))
